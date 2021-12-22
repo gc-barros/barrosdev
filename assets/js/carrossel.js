d09cfca58c61e0a29ee8d7afecf8cb948608e6a6
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-12-21 23:52:32 -0300

    Renderização dos detalhes dos projetos

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,21 +1,72 @@
 function carrosselInit() {
     const botaoAnterior = "[data-botao-anterior]";
     const botaoProximo = "[data-botao-proximo]";
-    let listaProjetos = "[data-carrossel]";
-    let navegacao = "[data-navegacao]";
+    const listaProjetos = "[data-carrossel]";
+    const navegacao = "[data-navegacao]";
+    const titulo = "[data-projeto='titulo']"
+    const link = "[data-projeto='link']"
+    const detalhes = "[data-projeto='detalhes']"
 
-    let carrossel = new Carousel(botaoAnterior, botaoProximo, listaProjetos, navegacao);
-    console.log(carrossel);
+    const dadosProjetos = {
+        bluewand: [
+            {
+                titulo: "Blue Wand",
+                link: "https://bluewand.vercel.app",
+                detalhes:
+                    "Landing page da Blue Wand, um grupo 100% maranhense de apoio a homens acometidos por câncer de pênis. Projeto desenvolvido em grupo, durante o primeiro desafio integrado do Programa Trilhas (Inova Maranhão).",
+            }
+        ],
+        porelas: [
+            {
+                titulo: "Por Elas",
+                link: "https://porelas.ong.br",
+                detalhes:
+                    "Website do Por Elas, um projeto fantástico que mobiliza pessoas e realiza ações para mulheres em São Luís – MA. Landing page construída por um grupo de programadores do Programa Trilhas (Inova Maranhão).",
+            }
+        ],
+        eguassiow: [
+            {
+                titulo: "Éguas, Siow!",
+                link: "https://eguassiow.netlify.app",
+                detalhes:
+                    'Site desenvolvido de forma individual, durante o 1º Workshop Trilhas, com o objetivo de promover a cultura do Estado do Maranhão, exaltando as suas belezas, encantos e diversidade.'
+            }
+        ],
+        petplanet: [
+            {
+                titulo: "Pet Planet",
+                link: "https://petplanet.netlify.app/",
+                detalhes:
+                    'Projeto de uma clínica petshop fictícia, com telas de início, quem somos, serviços, fale conosco, login e cadastro. Desenvolvido em grupo, durante a disciplina de "Projeto e Desenvolvimento de Software" (somente front-end).',
+            }
+        ],
+        petplanetuser: [
+            {
+                titulo: "Pet Planet | Usuário",
+                link: "https://petplanet.netlify.app/07-user-page.html",
+                detalhes:
+                    "Páginas internas da clínica Pet Planet, para as quais os usuários seriam direcionados após o login, com acesso a telas navegáveis de início, produtos, serviços e agenda (back-end não integrado).",
+            }
+        ],
+    };
+
+    let carrossel = new Carousel(botaoAnterior, botaoProximo, listaProjetos, navegacao, titulo, link, detalhes, dadosProjetos);
+
 }
 
 export default carrosselInit;
 
 class Carousel {
-    constructor(anterior, proximo, listaProdutos, navegacao) {
+    constructor(anterior, proximo, listaProdutos, navegacao, titulo, link, detalhes, dados) {
         this.anterior = document.querySelector(anterior);
         this.proximo = document.querySelector(proximo);
         this.listaProdutos = document.querySelector(listaProdutos);
         this.navegacao = document.querySelector(navegacao);
+
+        this.titulo = document.querySelector(titulo);
+        this.link = document.querySelector(link);
+        this.detalhes = document.querySelector(detalhes);
+        this.dados = dados;
 
         this.slides = this.getListaSlides();
         this.indicadores = this.getListaIndicadores();
@@ -79,6 +130,8 @@ class Carousel {
 
         this.scrollParaSlide(this.getSlideAtual());
         this.atualizaIndicadores(indicadorAtual, indicadorSelecionado);
+
+        this.renderizarDescricao();
     }
 
     scrollParaSlide(slideSelecionado) {
@@ -103,5 +156,16 @@ class Carousel {
         this.slides.forEach((slide, i) => {
             slide.style.left = this.tamanhoSlide * i + "px";
         });
+
+        this.renderizarDescricao();
+    }
+
+    renderizarDescricao() {
+        const lista = ['bluewand', 'porelas', 'eguassiow', 'petplanet', 'petplanetuser']
+        this.titulo.innerText = this.dados[lista[this.indiceDoSlideAtual]][0]['titulo'];
+        let linkProjeto = this.dados[lista[this.indiceDoSlideAtual]][0]['link']
+        this.link.innerText = linkProjeto;
+        this.link.setAttribute('href', linkProjeto);
+        this.detalhes.innerText = this.dados[lista[this.indiceDoSlideAtual]][0]['detalhes'];
     }
 }
