5a3113334903fb6430e8ecf12d806339c22564a3
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-12-23 13:45:46 -0300

    Atualizando botões

--- assets/js/linksInternos.js
+++ assets/js/linksInternos.js
@@ -2,37 +2,48 @@ function linksInternosInit() {
     const botaoContato = document.querySelectorAll("[data-botao='contato']");
     const botaoSobre = document.querySelectorAll("[data-botao='sobre']");
     const botaoProjetos = document.querySelectorAll("[data-botao='projetos']");
+    const botaoHabilidades = document.querySelectorAll("[data-botao='habilidades']");
 
     const sectionContato = document.getElementById('contato');
     const sectionSobre = document.getElementById('sobre');
     const sectionProjetos = document.getElementById('projetos');
+    const sectionHabilidades = document.getElementById('habilidades');
 
     botaoSobre.forEach((botao) => {
         botao.addEventListener(
-            "click", () => {
-                scrollToIdOnClick(sectionSobre)
+            "click", (event) => {
+                event.preventDefault();
+                scrollToIdOnClick(sectionSobre.offsetTop - 50);
             }
         );
     });
-
+        
     botaoProjetos.forEach((botao) => {
         botao.addEventListener(
-            "click", () => {
-                scrollToIdOnClick(sectionProjetos)
+            "click", (event) => {
+                event.preventDefault();
+                scrollToIdOnClick(sectionProjetos.offsetTop - 50);
             }
         );
     });
-
+            
     botaoContato.forEach((botao) => {
         botao.addEventListener(
-            "click", () => {
-                scrollToIdOnClick(sectionContato)
+            "click", (event) => {
+                event.preventDefault();
+                scrollToIdOnClick(sectionContato.offsetTop + 40);
             }
         );
     });
 
-    function scrollToIdOnClick(section) {
-        let sectionHeight = section.offsetTop - 40;
+    botaoHabilidades.forEach((botao) => {
+        botao.addEventListener("click", (event) => {
+            event.preventDefault();
+            scrollToIdOnClick(sectionHabilidades.offsetTop - 50);
+        });
+    });
+
+    function scrollToIdOnClick(sectionHeight) {
         document.body.scrollTop = sectionHeight; // Safari
         document.documentElement.scrollTop = sectionHeight; // Chrome, Firefox, IE e Opera
     }
